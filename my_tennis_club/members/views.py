fa45c07642251b7dfc0d3b2a8670a8af55111eb6
frashid17 <frashid274@gmail.com>
2024-10-20 13:20:20 +0300

    modified view for the new template

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,9 +1,14 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse #this is used to send a response back to the users browsers
+from django.template import loader #loader is used to load HTML templates in this case (all_members.html)
+from .models import Member #this refers to a model representing a db table where data about members is stores
 
-def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+def members(request): #this is a view function that handles a request when someone visits a specific URL
+    mymembers = Member.objects.all().values() #fetches all the rows from the Members table in the database
+    template = loader.get_template('all_members.html') #this line loads an html file called all_members.html from the templates folder
+    context = { #the context is a dictionary that stores variables you want to pass to the template.
+        'mymembers' : mymembers, # here the key 'mymembers' holds all the member data
+    }
+    return HttpResponse(template.render(context, request)) #this line takes the context renders it into the html template and sends result ans an HTTP response to the user's browser
 
 
 
