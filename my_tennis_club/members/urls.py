18c698b7bc1f12faad0462e180efe13e8e9ee55d
frashid17 <frashid274@gmail.com>
2024-10-20 19:38:33 +0300

    added testing view

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('members/', views.members, name='members'),  # URL for viewing all members
     path('members/details/<int:id>/', views.details, name='details'),  # Nested URL for viewing member details
+    path('testing', views.testing, name='testing'),
 ]
