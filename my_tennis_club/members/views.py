890abd82950b587666a2ca60175ce29b4f71bbfe
frashid17 <frashid274@gmail.com>
2024-10-21 14:52:36 +0300

    modified some changes

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -24,9 +24,10 @@ def main(request):
     return HttpResponse(template.render())
 
 def testing(request):
+    mymembers = Member.objects.all().values()
     template = loader.get_template('template.html')
     context = {
-        'firstname': 'Patrick',
+        'mymembers': mymembers,
     }
     return HttpResponse(template.render(context, request))
 
